b249284fdee8c6b8c4a5f1798cdac79fad0ee67d
Luca Ceriani <luca.ceri@gmail.com>
2021-06-08 15:42:38 +0200

    v1.0.0

--- public/index.js
+++ public/index.js
@@ -1,22 +1,23 @@
-if (location.pathname.length > 4) {
-    let redirect = new OOO().decodeUrl(decodeURI(location.pathname.replace("/", "")))
-    // location.href = redirect
-    console.log(redirect);
-
-    let metaEl = document.createElement('meta')
-    metaEl.httpEquiv = 'refresh'
-    metaEl.content = '0;url=' + redirect
-    document.head.appendChild(metaEl);
-
-}
+const input = document.getElementById("input-url")
+const output = document.getElementById("output-url")
 
 function oooify() {
-    const input = document.getElementById("input-url")
-    const output = document.getElementById("output-url")
-    // const domain = "ooooooooooooooooooooooo.ooo"
     const domain = location.host
-
     const ooo = new OOO()
-    const url = ooo.encodeUrl(input.value)
-    output.innerHTML = `<a href="${location.protocol}//${domain}/${url}">${domain}/${url}</a>`
+
+    let url = ooo.encodeUrl(input.value)
+    url = `${location.protocol}//${domain}/${url}`
+
+    output.innerHTML = `<a href="${url}">${url}</a><br><p>Copied to clipboard!</p>`
+
+    copy(url)
 }
+
+function copy(str) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
