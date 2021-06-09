37707215ad63acf23e6fc4e600c6e66f36dc3cbb
Luca Ceriani <luca.ceri@gmail.com>
2021-06-09 13:20:17 +0200

    v1.0.4

--- public/index.js
+++ public/index.js
@@ -1,11 +1,40 @@
 const input = document.getElementById("input-url")
 const output = document.getElementById("output-url")
 
-function oooify() {
-    const domain = location.host
-    const ooo = new OOO()
+// make tooltips
+tippy(
+    "#copy-button",
+    {
+        content: "Copied to clipboard!",
+        trigger: "click",
+        animation: "shift-away-subtle"
+    }
+)
 
-    let url = ooo.encodeUrl(input.value)
+function oooify() {
+
+    try {
+        new URL(input.value)
+    } catch (e) {
+        input.style.opacity = 1
+
+        let oldValue = input.value
+        input.value = "Invalid URL! Noooooooooooo"
+
+        let times = 0
+        let i = setInterval(async () => {
+            input.style.opacity = (parseInt(input.style.opacity) + 1) % 2
+            if (++times == 6) {
+                clearInterval(i)
+                input.value = oldValue
+            }
+        }, 100)
+        return
+    }
+
+    const domain = location.host
+
+    let url = new OOO().encodeUrl(input.value.trim())
     url = `${location.protocol}//${domain}/${url}`
 
     document.getElementById("output-div").classList.remove("d-none")
@@ -23,4 +52,7 @@ function copy() {
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
+
+    setTimeout(() => tippy.hideAll(), 2000)
+
 };
